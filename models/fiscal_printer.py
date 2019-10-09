e95a34d738ae33e8189077520b846f097b972223
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-10-09 11:59:02 -0500

    [REF]

--- models/fiscal_printer.py
+++ models/fiscal_printer.py
@@ -1,6 +1,9 @@
 from odoo import api, models
 
 
+CHUNK_SIZE = 24
+
+
 def commafile(value):
     '''Return numbers with comma instead of decimal point.'''
     return str(value).replace('.', ',')
@@ -38,7 +41,7 @@ class FiscalPrinter(models.TransientModel):
             # TODO IMP check , instead of .
             vat_id = product.taxes_id[0]  # TODO IMP multiple taxes
             vat_code = pos_config.fp_tax_ids.search([('tax_id', '=', vat_id.id)]).code
-            if row['qty'] == 1:
+            if row['qty'] == 1:  # Ignore qty if is == 1
                 content += '1021;1;;{vat};;;{name};{price}\n'.format(vat=vat_code, name=row['name'], price=commafile(row['price']))
             else:
                 content += '1021;1;;{vat};;;{name};{price};{qty}\n'.format(vat=vat_code, name=row['name'], price=commafile(row['price']), qty=commafile(row['qty']))
@@ -46,7 +49,7 @@ class FiscalPrinter(models.TransientModel):
             additional_text = ''
             for field in pos_config.fp_product_additional_text_ids:
                 additional_text += product[field.name].name
-            for chunk in to_chunks(additional_text, 24):
+            for chunk in to_chunks(additional_text, CHUNK_SIZE):
                 content += '112;{};0;1;3\n'.format(chunk)
             # PRODUCT DISCOUNT
             if row.get('discount'):
@@ -71,7 +74,7 @@ class FiscalPrinter(models.TransientModel):
         content += '1030;{payment_code};1;;{cash}\n'.format(payment_code=payment_code, cash=commafile(cash))
         # ADDITIONAL TEXT
         additional_text = pos_config.fp_additional_text
-        for chunk in to_chunks(additional_text, 24):
+        for chunk in to_chunks(additional_text, CHUNK_SIZE):
             content += '112;{};0;1;3\n'.format(chunk)
         # CUTTER RECEIPT PAPER
         cutter_receipt_paper = pos_config.fp_cutter_receipt_paper
